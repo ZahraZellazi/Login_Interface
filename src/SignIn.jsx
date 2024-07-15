49618ebbf2a9dcd5abe6c5f4a1ee921d366850f3
ZahraZellazi <zahra.zellazi@esprit.tn>
2024-07-15 12:37:05 +0100

    login_interface_css_fini

--- src/SignIn.jsx
+++ src/SignIn.jsx
@@ -4,18 +4,25 @@ import './SignIn.css';
 
 const SignIn = () => {
   return (
-    <div className="container">
-        <div className='title'>Sing In</div>
-      <div className="social-container">
-        <a href="https://www.facebook.com/" className="social"><FaFacebook /></a>
-        <a href="https://accounts.google.com/v3/signin/confirmidentifier?authuser=0&continue=https%3A%2F%2Faccounts.google.com%2F&followup=https%3A%2F%2Faccounts.google.com%2F&ifkv=AdF4I76to9BgK28MnX5qXY7rlXPUBtAIf_8XOAgh1-agRrJYuja6glgt1Ll7AeJO5_Rlw7bYR8gcRA&passive=1209600&flowName=GlifWebSignIn&flowEntry=ServiceLogin&dsh=S1829618674%3A1721034831493725&ddm=0" className="social"><FaGoogle /></a>
-        <a href="https://www.linkedin.com/" className="social"><FaLinkedin /></a>
+    <div className='wrapperI'>
+    <div className="containerI">
+        <div className='titleI'>Sing In</div>
+      <div className="social-containerI">
+        <a href="*" className="socialI facebook"><FaFacebook /></a>
+        <a href="*" className="socialI google"><FaGoogle /></a>
+        <a href="*" className="socialI linkedin"><FaLinkedin /></a>
       </div>
-      <div className="form-container">
-        <input type="text" placeholder="E-mail" className="input" />
-        <input type="password" placeholder="Password" className="input" />
+      <div className="form-containerI">
+        <input type="text" placeholder="E-mail" className="inputI" />
+        <input type="password" placeholder="Password" className="inputI" />
         <a href="/forgot-password" className="link">Forgot Password?</a>
-        <button className="button">Sign Up</button>
+        <button className="buttonI">Sign Up</button>
+      </div>
+    </div>
+    <div className="containerII">
+        <div className="titleII">Welcome !</div>
+        <p className='paragII'> To create an account please create an account with your personal info.</p>
+        <button className="buttonII">Sign In</button>
       </div>
     </div>
   );
--- src/SignUp.jsx
+++ src/SignUp.jsx
@@ -4,25 +4,26 @@ import "./SignUp.css"
 
 function SignUp() {
   return (
-    <div className='main-container'>
-    <div className="container">
-        <div className='title'>Sing Up</div>
-      <div className="social-container">
-        <a href="https://www.facebook.com/" className="social"><FaFacebook /></a>
-        <a href="https://accounts.google.com/v3/signin/confirmidentifier?authuser=0&continue=https%3A%2F%2Faccounts.google.com%2F&followup=https%3A%2F%2Faccounts.google.com%2F&ifkv=AdF4I76to9BgK28MnX5qXY7rlXPUBtAIf_8XOAgh1-agRrJYuja6glgt1Ll7AeJO5_Rlw7bYR8gcRA&passive=1209600&flowName=GlifWebSignIn&flowEntry=ServiceLogin&dsh=S1829618674%3A1721034831493725&ddm=0" className="social"><FaGoogle /></a>
-        <a href="https://www.linkedin.com/" className="social"><FaLinkedin /></a>
+    <div className='wrapperU'>
+    <div className="containerU">
+        <div className='titleU'>Sign Up</div>
+      <div className="social-containerU">
+        <a href="*" className="socialU facebook"><FaFacebook /></a>
+        <a href="*" className="socialU google"><FaGoogle /></a>
+        <a href="*" className="socialU linkedin"><FaLinkedin /></a>
       </div>
-      <div className="form-container">
-        <input type="text" placeholder="Name" className="input" />
-        <input type="text" placeholder="E-mail" className="input" />
-        <input type="password" placeholder="Password" className="input" />
+      <div className="form-containerU">
+        <input type="text" placeholder="Name" className="inputU" />
+        <input type="text" placeholder="E-mail" className="inputU" />
+        <input type="password" placeholder="Password" className="inputU" />
         <a href="/forgot-password" className="link">Forgot Password?</a>
-        <button className="button">Sign Up</button>
+        <button className="buttonU">Sign Up</button>
       </div>
     </div>
-    <div className="container">
-        <div className="title">Second Container</div>
-        <p>This is the second container where you can add more content or functionalities.</p>
+    <div className="containerUU">
+        <div className="titleUU">Have An Account !</div>
+        <p className='paragUU'> To connected with us please login with your personal info.</p>
+        <button className="buttonUU">Sign In</button>
       </div>
     </div>
 
